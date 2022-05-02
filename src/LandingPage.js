709a014ec3c29751da220461c5b1426ccac227ef
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2022-05-02 10:58:30 -0400

    added react bootstrap and started desinging the landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Container,Row } from 'react-bootstrap'
 
 function LandingPage() {
   return (
-    <div>LandingPage</div>
+    <div>
+    <h2>Welcome to LULU ISLAND!</h2>
+    <h5>Destination:</h5>
+    <Row style={{width:'90vw', height:'auto',marginLeft:'auto', marginRight:'auto', border:'2px solid yellow'}}>
+    <Container style={{width:'50%', height:'50%', border:'2px solid black'}}>
+    All art
+    </Container>
+    <Container style={{width:'50%', height:'50%', border:'2px solid red'}}>
+    New Art
+    </Container>
+    <Container style={{width:'50%', height:'50%', border:'2px solid blue'}}>
+    Curated Art
+    </Container>
+    <Container style={{width:'50%', height:'50%', border:'2px solid green'}}>
+    artist alley
+    </Container>
+    </Row>
+    </div>
   )
 }
 
